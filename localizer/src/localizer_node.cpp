16bbf10777e499743764c7a0b2ccf574dfca0806
Hidaka Aoi <ee213030@meiji.ac.jp>
2024-03-14 15:04:38 +0900

    hidaka1

---
+++ localizer/src/localizer_node.cpp
@@ -0,0 +1,12 @@
+#include "localizer/localizer.hpp"
+
+// ===== メイン関数 =====
+int main(int argc, char* argv[])
+{
+    // ros::init(argc, argv, "localizer"); 
+    rclcpp::init(argc, argv, "localizer"); // ノードの初期化
+    EMCL emcl;
+    emcl.process();
+
+    return 0;
+}

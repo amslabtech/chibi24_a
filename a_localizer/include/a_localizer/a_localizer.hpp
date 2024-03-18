2e3ca7189700d100a8637c76b4b45380725d7129
Hidaka Aoi <ee213030@meiji.ac.jp>
2024-03-18 13:42:33 +0900

    a_localizer

--- a_localizer/include/a_localizer/a_localizer.hpp
+++ a_localizer/include/a_localizer/a_localizer.hpp
@@ -23,7 +23,7 @@ emcl: mcl with expansion resetting
 #include <tf2/utils.h>
 #include <tf2/convert.h>
 #include <tf2_ros/transform_broadcaster.h>
-#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
 
 #include <random>
 #include <functional>
